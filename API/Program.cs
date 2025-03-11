b3b0ba579d00cfea987e12e75d6a725a142431d0
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-03-11 13:16:24 +0530

    Created a connection string and created a database using EF

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//This adds MVC and Web API controllers
 builder.Services.AddControllers();
+//adding the DataContext class (of type DbContext) in the dependency injection container
+//to configure SQLite database with a connection string from app configuration
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
