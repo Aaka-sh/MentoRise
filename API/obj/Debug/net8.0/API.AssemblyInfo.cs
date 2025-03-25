65719ff107ddf04cd68d630d93992edb95ff5fdc
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-03-25 11:16:50 +0530

    Added the register component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5092976919ac19480d63517d543c62f4e32e50a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962c200e6e3daf3b956966d72b5f332d95db7bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
