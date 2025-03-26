5163e630e842e2c6716ab75713e3dcb422f6c3fa
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-03-26 16:49:05 +0530

    Added routing and created a few components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d093a387d8a77629298e66abef09b8c5713bd2f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea5e7d128f98f604c0a1c208322b988acb359e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
