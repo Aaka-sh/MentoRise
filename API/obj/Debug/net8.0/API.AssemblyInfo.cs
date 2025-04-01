6d21ce117a2ab9f568f53fbe1e1d895ca15cc9f9
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-04-01 16:49:12 +0530

    Created member cards using bootstrap

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a433618fc5fce6a5a8024ef83e37f5027d664bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8125000365b05850738ec3df37a43294b128700")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
