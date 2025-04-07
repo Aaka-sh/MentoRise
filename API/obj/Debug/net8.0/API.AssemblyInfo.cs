aec25607b6e7662eba13e8064a4f8e75f11e1107
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-04-07 12:09:46 +0530

    Added the update functionality in the member forms

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c90cf6954cc8e0a7d1570f8412fd5e6dcf1f61c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315a016bd866a50bb2e572b4b6b244535e0a90cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
