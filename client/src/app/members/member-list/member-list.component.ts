385fda6ee3c70b7873083abbef273a06f5566d04
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-04-21 17:07:46 +0530

    Angular bootstrap Pagination component

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/member.service';
-import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
@@ -19,5 +19,11 @@ export class MemberListComponent implements OnInit {
 
   loadMembers() {
     this.memberService.getMembers(this.pageNumber, this.pageSize);
+  }
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
