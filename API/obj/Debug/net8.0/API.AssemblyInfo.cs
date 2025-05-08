b031f3b2ed499dffff482d7ca6c054db832358cf
Aaka-sh <akashgoyal@mca.christuniversity.in>
2025-05-08 16:27:12 +0530

    Added styling to the message thread

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3825008900578ed4937c49c03fafdab6ba890a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccae1ed6fb77bf7d346a86500c41a6938c5db55f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
